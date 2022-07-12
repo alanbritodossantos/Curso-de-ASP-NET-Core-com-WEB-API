4497f2df8cecb5dd17855ffb9d91cd65b8682152
Alan <alanbritodossantos2015@gmail.com>
2022-07-11 22:30:30 -0300

    criando classe DbContext para o banco

---
+++ APIIntroducao/APIIntroducao/Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIIntroducao.Models
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+    }
+}
--- APIIntroducao/APIIntroducao/Startup.cs
+++ APIIntroducao/APIIntroducao/Startup.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using APIIntroducao.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -23,6 +25,9 @@ namespace APIIntroducao
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseInMemoryDatabase("produtoDB")
+            );
             services.AddMvc();
         }
 
